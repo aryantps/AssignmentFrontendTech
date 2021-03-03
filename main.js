769a60fc1a371417590d743054e926b6656a268a
Aryant Pratap Singh <46716746+aryantps@users.noreply.github.com>
2021-03-03 22:18:55 +0000

    added bootstrap form and table

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-alert('JS IS WORKING')
+// alert('JS IS WORKING')
+var enrolledName = document.getElementById('inputName');
+var email = document.getElementById('inputEmail');
+var website = document.getElementById('inputWebsite');
+var image = document.getElementById('inputImg');
+var gender = document.getElementsByName('gender');
+var skill = document.getElementsByName('skills')
